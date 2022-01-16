93e5d076097758fc5cd9c5a0037d0187136ec6cb
Edwin Adjei <code_ed381@outlook.com>
2022-01-16 11:41:06 +0000

    new commit

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+
+
+
+
 const book = document.getElementById('book').value
 
 console.log(book)
 
+
+const bookTitle = document.getElementById('bookName').value
+const author = document.getElementById('author').value
+const isbn = document.getElementById('isbn').value
+const btn = document.getElementById('btn')
 
